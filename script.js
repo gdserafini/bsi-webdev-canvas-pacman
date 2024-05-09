f494c8f9b8f878d2d4c914fc89232812996f03c3
victor-eidt <115123380+victor-eidt@users.noreply.github.com>
2024-05-09 16:57:40 -0300

    Classe limites (17:00)

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-function draw(context){
-    //TODO
-}
-
 if(!context) alert('Canvas ')
 else{
     //usa a altura e largura default do navegador
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     //console.log(canvas);
-    draw(context);
     //console.log(context);
 }
+
+class Limite {
+    constructor({posicao}, ) {
+        this.posicao = posicao
+        this.width = 40
+        this.height = 40
+    }
+
+    draw() {
+        context.fillStyle = 'blue'
+        context.fillRect(this.posicao.x, this.posicao.y, this.width, this.height)
+    }
+}
+
+const limites = [
+    new Limite({
+        posicao: {
+            x: 0,
+            y: 0
+        }
+    }),
+    new Limite({
+        posicao: {
+            x: 41,
+            y: 0
+        }
+    })
+]
+
+limites.forEach(limite => {
+    limite.draw()
+})
