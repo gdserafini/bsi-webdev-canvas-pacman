47aeec2621eddc7de6fa24ae014306cb9098d7f9
gdserafini <gdinizserafini@gmail.com>
2024-05-15 19:33:04 -0300

    limites adicionados - mapa

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Projeto da diciplina de Web Development HTML5 Canvas & Games - Pacman
-O jogo possui uma tela inicial com as opções de jogar, sobre e visualizar a maior pontuação do jogo.
-Nesta implementação há duas fases com números váriados de inimigos.
+O jogo possui uma tela inicial com as opções de jogar, sobre e visualizar a maior pontuação do jogo. Nesta implementação há duas fases com números váriados de inimigos.
 
 ## Como rodar o projeto
 ...
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-if(!context) alert('Canvas ')
+if(!context) alert('Canvas - error')
 else{
     //usa a altura e largura default do navegador
     canvas.width = window.innerWidth;
@@ -11,33 +11,46 @@ else{
 }
 
 class Limite {
+    static largura = 40;
+    static altura = 40;
     constructor({posicao}, ) {
-        this.posicao = posicao
-        this.width = 40
-        this.height = 40
+        this.posicao = posicao;
+        this.width = 40;
+        this.height = 40;
     }
 
     draw() {
-        context.fillStyle = 'blue'
-        context.fillRect(this.posicao.x, this.posicao.y, this.width, this.height)
+        context.fillStyle = 'blue';
+        context.fillRect(
+            this.posicao.x, this.posicao.y, this.width, this.height
+        );
     }
 }
 
-const limites = [
-    new Limite({
-        posicao: {
-            x: 0,
-            y: 0
+const mapa = [
+    ['-','-','-','-','-','-'],
+    ['-',' ',' ',' ',' ','-'],
+    ['-',' ','-','-',' ','-'],
+    ['-',' ',' ',' ',' ','-'],
+    ['-','-','-','-','-','-']
+];
+const limites = [];
+
+mapa.forEach((linha, i) => {
+   linha.forEach((simbolo, j) => {
+        switch(simbolo){
+            case '-': 
+                limites.push(new Limite({
+                    posicao: {
+                        x: Limite.largura * j, 
+                        y: Limite.altura * i
+                    }
+                }))
+                break;
         }
-    }),
-    new Limite({
-        posicao: {
-            x: 41,
-            y: 0
-        }
-    })
-]
+   }) 
+});
 
 limites.forEach(limite => {
     limite.draw()
-})
+});
